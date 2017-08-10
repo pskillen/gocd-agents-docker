5f944ab3f2cb0c7fc986c41eefdce24fdc3aad3c
Patrick Skillen <pskillen@gmail.com>
2017-08-10 23:07:46 +0100

    Remove parameterisation

--- ubuntu/16.04/node6/build.sh
+++ ubuntu/16.04/node6/build.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-AGENT_VER=17.8.0
-
-TAG=pskillen/gocd-agent-ubuntu-16.04-node6.x:${AGENT_VER}
-docker build -t ${TAG} --build-arg AGENT_VER=${AGENT_VER} .
+TAG=pskillen/gocd-agent-ubuntu-16.04-node6.x:17.8.0
+docker build -t ${TAG} --build-arg AGENT_VER=17.8.0 .
 docker push ${TAG}
--- ubuntu/16.04/node8/build.sh
+++ ubuntu/16.04/node8/build.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-AGENT_VER=17.8.0
-
-TAG=pskillen/gocd-agent-ubuntu-16.04-node8.x:${AGENT_VER}
-docker build -t ${TAG} --build-arg AGENT_VER=${AGENT_VER} .
+TAG=pskillen/gocd-agent-ubuntu-16.04-node8.x:17.8.0
+docker build -t ${TAG} --build-arg AGENT_VER=17.8.0 .
 docker push ${TAG}
